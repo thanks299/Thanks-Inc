d753199fb5e17094f111502143d1792577b4ac31
thanks299 <139892876+thanks299@users.noreply.github.com>
2025-01-15 23:35:51 +0100

    Footer.tsx page updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Nexus Group. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Thanks Group Inc.  All rights reserved.</p>
         </div>
       </div>
     </footer>
